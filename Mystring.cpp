20b26196af3e7f2ef538ffea118ab76b12646024
SajjadShafay <shafaysajjad@outlook.com>
2023-03-24 14:14:54 +0000

    adding comments

--- Mystring.cpp
+++ Mystring.cpp
@@ -3,10 +3,10 @@
 #include <cstring>
 
 //no-args constructor
-Mystring::Mystring() : str{ nullptr }
+Mystring::Mystring() : str{ nullptr } // initialise the objects str pointer to null 
 {
-	str = new char[1];
-	*str = '\0';
+	str = new char[1];  // point to a new char array that will hold a string terminator
+	*str = '\0'; // string terminator
 }
 
 //overloaded constructor
@@ -14,20 +14,20 @@ Mystring::Mystring(const char* s) : str{ nullptr }
 {
 	if (s == nullptr)
 	{
-		str = new char[1];
-		*str = '\0';
+		str = new char[1];          // if s is a pointer pointing to null
+		*str = '\0'; 
 	}
 	else
 	{
-		str = new char[strlen(s) + 1];
-		strcpy(str, s);
+		str = new char[strlen(s) + 1];  // create a new char array that is 1 more than the size of s
+		strcpy(str, s); // copy the contents of s into str. 
 	}
 }
 
 //Copy constructor
 Mystring::Mystring(const Mystring& source) : str{ nullptr }
 {
-	str = new char[strlen(source.str) + 1];
+	str = new char[strlen(source.str) + 1];   
 	strcpy(str, source.str);
 	std::cout << "Copy constructor used" << std::endl;
 }
@@ -50,24 +50,24 @@ Mystring& Mystring::operator= (const Mystring& rhs)
 {
 	std::cout << "Using copy assignment" << std::endl;
 
-	if (this == &rhs)
+	if (this == &rhs)  // check for self assignment
 		return *this;
-	delete[] str;
-	str = new char[strlen(rhs.str) + 1];
-	strcpy(str, rhs.str);
-	return *this;
+	delete[] str;  // empty out the contents of str
+	str = new char[strlen(rhs.str) + 1]; // set str to a new char that is the length of rhs.str + 1 for the string terminator
+	strcpy(str, rhs.str); // copy the contents of rhs.str into str 
+	return *this; // return the actual object. 
 }
 
 //Move assignment
-Mystring& Mystring::operator= (Mystring&& rhs)
+Mystring& Mystring::operator= (Mystring&& rhs) 
 {
-	std::cout << "Using move assignment" << std::endl;
+	std::cout << "Using move assignment" << std::endl;  // 
 	if (this == &rhs)
-		return *this;
-	delete[] str;
-	str = rhs.str;
-	rhs.str = nullptr;
-	return *this;
+		return *this;   // check if the address of this object is the same as the address for the rhs object for self assignment
+	delete[] str; // empty out the contents of str
+	str = rhs.str; // set the str of this object to the same value of the str of the rhs object 
+	rhs.str = nullptr; // set the str pointer of the rhs object to null 
+	return *this; // return this actual object
 }
 
 //Display method
--- Mystring.h
+++ Mystring.h
@@ -1,38 +1,38 @@
 #ifndef _MYSTRING_H_
 #define _MYSTRING_H_
-#include <ostream>
-#include <istream>
+#include <ostream>     // Include to make std::ostream work
+#include <istream>     // Include to make std::istream work
 
 class Mystring
 {
-	friend std::ostream& operator<<(std::ostream& os, const Mystring& rhs);
-	friend std::istream& operator>>(std::istream& in, Mystring& rhs);
+	friend std::ostream& operator<<(std::ostream& os, const Mystring& rhs);   // friend function passes output stream by value because we are changing it and want it to stay changed. 
+	friend std::istream& operator>>(std::istream& in, Mystring& rhs); // same as above but rhs is not const because we want to change the rhs variable. 
 private:
-	char* str;  // pointer to a char[] that holds a C-style string
+	char* str;  // pointer to a char[] that holds a C-style string. Points to the first character of a string. If you dereference then you will only get that first character
 public:
 	Mystring();  // No-args constructor
-	Mystring(const char* s);  // overloaded constructor
-	Mystring(const Mystring& source); // Copy constructor
+	Mystring(const char* s);  // overloaded constructor  // normal c-style string that we don't want to change
+	Mystring(const Mystring& source); // Copy constructor 
 	Mystring(Mystring&& source); // Move constructor
 	~Mystring(); // Destructor
 
-	Mystring& operator=(const Mystring& rhs); // copy assignment
-	Mystring& operator=(Mystring&& rhs); // Move assignment
+	Mystring& operator=(const Mystring& rhs); // copy assignment - returns the address of a Mystring object
+	Mystring& operator=(Mystring&& rhs); // Move assignment - returns the address of a Mystring object
 
 	void display() const;
 
 	int get_length() const;
 	const char* get_str() const;
 
-	Mystring operator- () const; 
-	bool operator== (const Mystring& rhs) const;
-	bool operator!= (const Mystring& rhs) const;
-	bool operator< (const Mystring& rhs) const;
-	bool operator> (const Mystring& rhs) const;
-	Mystring operator+ (const Mystring& rhs) const;
-	Mystring& operator+= (const Mystring& rhs); 
-	Mystring operator* (int num) const;
-	Mystring& operator*= (int num); 
+	Mystring operator- () const; // this is the unary - operator. If there was something in the brackets then it would be the binary version. Returns a Mystring object. 
+	bool operator== (const Mystring& rhs) const; // is a bool because it returns true or false. Just a comparison. ends with const because we are changing nothing. 
+	bool operator!= (const Mystring& rhs) const; // same as above
+	bool operator< (const Mystring& rhs) const; // same as above
+	bool operator> (const Mystring& rhs) const; // same as above
+	Mystring operator+ (const Mystring& rhs) const; // Returns a Mystring object. Const because we will store the result in a new object and not change either of the ones used. 
+	Mystring& operator+= (const Mystring& rhs); // Self assignment. 
+	Mystring operator* (int num) const; // Returns a Mystring object. Const because we will store the result in a new object and not change either of the ones used. 
+	Mystring& operator*= (int num); // Self assignment
 };
 
 #endif // _MYSTRING_H_
